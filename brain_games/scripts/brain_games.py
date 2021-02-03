a2e9f3a3fe0e8d5f97155c504b5c74304cf1d7e3
Dmitriy Knyazev <dimsknz@gmail.com>
2021-02-03 18:00:55 +0700

    feat: add nameasking

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import sys
+
+from brain_games import cli
 
 def main():
     if __name__ == '__main__':
         main()
 print('Welcome to the Brain Games!')
-
+cli.welcome_user()
